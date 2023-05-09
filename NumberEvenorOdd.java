25f2b83bf33b8b406a0766f1a2646f4187ffd065
SAAD HOSSAIN <132562957+hossainsaad@users.noreply.github.com>
2023-05-09 17:05:57 +0600

    Add files via upload

---
+++ NumberEvenorOdd.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class NumberEvenorOdd {
+    public static void main(String[] args) {
+//
+//        Scanner l = new Scanner(System.in);
+////        System.out.println("Enter your favourite Marks : ");
+////        a = l.nextInt();
+////        if (a%2==0){
+////            System.out.println("Number is Even");
+////        }
+////        else{
+////            System.out.println(" Number is Odd");
+////        }
+////    }
+////}
+//            double subject1,subject2,subject3,subject4, subject5,subject6,total,percentense;
+//        System.out.println("Enter your Marks 1: ");
+//    subject1 =l.nextDouble();
+//        System.out.println("Enter your Marks 2: ");
+//        subject2 = l.nextDouble();
+//        System.out.println("Enter your Marks 3: ");
+//        subject3 =l.nextDouble();
+//        System.out.println("Enter your Marks 4: ");
+//        subject4 = l.nextDouble();
+//        System.out.println("Enter your Marks 5: ");
+//        subject5 =l.nextDouble();
+//        System.out.println("Enter your Marks 6: ");
+//        subject6 = l.nextDouble();
+//        total = subject1+ subject2 +subject3+subject4+ subject5+subject6;
+//        percentense = (total/600)*100;
+//        if ( percentense>=90){
+//            System.out.println(" Grade A _Tremendous ");
+//        } else if (percentense<90 && percentense>=80) {
+//            System.out.println(" Talented _ Grade B");
+//        } else if (percentense<80 && percentense>=70) {
+//            System.out.println("Good boy_ your grade is B");
+//        } else if (percentense<70 && percentense>=60) {
+//            System.out.println("you passed but not good at all-your grade D");
+//
+//        }
+//        else{
+//            System.out.println("Opps you have failed");
+//    }
+//
+//    }
+//    }
